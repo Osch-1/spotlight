2d4b7ff5703731aeaec462d2ce8c075a2c122a02
daniil.gerasimov <daniil.gerasimov@travelline.ru>
2025-02-02 16:59:21 +0300

    feat: add build configs

---
+++ Infrastructure/SpotlightDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Foundation.Database.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class SpotlightDbContext : DbContext
+{
+    public SpotlightDbContext()
+        : base()
+    {
+    }
+
+    protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+    {
+        base.OnConfiguring( optionsBuilder );
+
+        // говорим что будем использовать sql server и указываем строку подключения
+        // при этом база данных на момент начала работы может не существовать
+        optionsBuilder.UseSqlServer( "Server=localhost\\SQLEXPRESS;Database=Spotlight;Trusted_Connection=True;TrustServerCertificate=True;" );
+    }
+
+    // Здесь передаем конфигурации сущностей <-> таблиц
+    protected override void OnModelCreating( ModelBuilder modelBuilder )
+    {
+        base.OnModelCreating( modelBuilder );
+
+        modelBuilder.ApplyConfiguration( new ActorConfiguration() );
+        modelBuilder.ApplyConfiguration( new PlayConfiguration() );
+        modelBuilder.ApplyConfiguration( new TheaterConfiguration() );
+    }
+}
